bf0d3cf3fd21e4e8334db4967950da91befbb956
Martin Durant <martin.durant@utoronto.ca>
2020-11-11 12:44:57 -0500

    more

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from os.path import exists
 from setuptools import setup
 
 setup(
@@ -7,6 +8,11 @@ setup(
     url='https://github.com/intake/fsspec-reference-maker',
     license='MIT',
     author='Martin Durant',
-    author_email='martin.duranbt@utoronto.ca',
-    description='Functions to make reference descriptions for ReferenceFileSystem'
+    author_email='martin.durant@utoronto.ca',
+    description='Functions to make reference descriptions for ReferenceFileSystem',
+    python_requires='>=3.7',
+    long_description=(open('README.md').read() if exists('README.md') else ''),
+    long_description_content_type='text/markdown',
+    install_requires=list(open('requirements.txt').read().strip().split('\n')),
+    zip_safe=False
 )
