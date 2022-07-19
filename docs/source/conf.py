290656c5327f9cab0f0e881ff3dd81432f87e541
peterm790 <petermarsh790@gmail.com>
2022-07-19 12:35:45 +0200

    add ipython dependency to docs requirements and modify html_static_path to fix warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ autodoc_mock_imports = [
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 pygments_style = "sphinx"
 html_theme = "sphinx_rtd_theme"
 html_logo = "kerchunk.png"
