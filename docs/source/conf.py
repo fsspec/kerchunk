f1c4577ba0c555131850401c2ee7da5a9f4355a9
peterm790 <petermarsh790@gmail.com>
2022-07-27 14:57:00 +0200

    add pandoc workaround

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,3 +57,29 @@ html_static_path = []
 pygments_style = "sphinx"
 html_theme = "sphinx_rtd_theme"
 html_logo = "kerchunk.png"
+
+
+from inspect import getsourcefile
+
+# Get path to directory containing this file, conf.py.
+DOCS_DIRECTORY = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
+
+def ensure_pandoc_installed(_):
+    import pypandoc
+
+    # Download pandoc if necessary. If pandoc is already installed and on
+    # the PATH, the installed version will be used. Otherwise, we will
+    # download a copy of pandoc into docs/bin/ and add that to our PATH.
+    pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
+    # Add dir containing pandoc binary to the PATH environment variable
+    if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
+        os.environ["PATH"] += os.pathsep + pandoc_dir
+    pypandoc.ensure_pandoc_installed(
+        quiet=True,
+        targetfolder=pandoc_dir,
+        delete_installer=True,
+    )
+
+
+def setup(app):
+    app.connect("builder-inited", ensure_pandoc_installed)
