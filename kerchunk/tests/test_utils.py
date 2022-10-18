3eb930c3159efbd0bdd7451a2eaefaba1ffef780
Martin Durant <martin.durant@alumni.utoronto.ca>
2022-10-18 15:11:20 -0400

    add a test

--- kerchunk/tests/test_utils.py
+++ kerchunk/tests/test_utils.py
@@ -23,3 +23,21 @@ def test_rename_files(m):
     assert out == b'{"version":1,"refs":{"v0":["newpath",0,0],"bin":"data"}}'
     out = m.cat("ref2.json")
     assert out == b'{"version":1,"refs":{"v0":["newerpath",0,0],"bin":"data"}}'
+
+
+def test_inline(m):
+    m.pipe("data", b"stuff")
+    refs = {
+        "key0": b"00",
+        "key1": ["memory://data"],
+        "key2": ["memory://data", 1, 1],
+        "key1": ["memory://data", 2, 4],
+    }
+    out = kerchunk.utils._do_inline(refs, 2)
+    expected = {
+        "key0": b"00",
+        "key1": ["memory://data"],
+        "key2": b"t",
+        "key1": ["memory://data", 2, 4],
+    }
+    assert out == expected
