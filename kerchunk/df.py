9a30f077d92dd87540ec6e11f4c42ff0491faddc
Alex Goodman <agoodm@users.noreply.github.com>
2023-03-10 19:50:58 -0600

    support categorical encoding

--- kerchunk/df.py
+++ kerchunk/df.py
@@ -96,11 +96,9 @@ def _write_json(fname, json_obj, storage_options=None):
 def refs_to_dataframe(
     refs,
     url,
-    consolidated=True,
     storage_options=None,
-    # need to balance speed to read one RG versus latency in many reads and size of
-    # parquet metadata. We should benchmark to remote storage to decide a good number
-    row_group_size=10_000,
+    record_size=10_000,
+    categorical_threshold=10,
     **kwargs,
 ):
     """Write references as a store of parquet files with multiple row groups.
@@ -115,14 +113,16 @@ def refs_to_dataframe(
     url: str
         Location for the output, together with protocol. This must be a writable
         directory.
-    consolidated : bool
-        Whether or not to add consolidated metadata key to references. (default True)
     storage_options: dict | None
         Passed to fsspec when for writing the parquet.
-    row_group_size : int
-        Number of references to store in each reference file (default 1000)
+    record_size : int
+        Number of references to store in each reference file (default 10000)
+    categorical_threshold : int
+        Encode urls as pandas.Categorical to reduce memory footprint if the ratio
+        of the number of unique urls to total number of refs for each variable
+        is greater than or equal to this number. (default 10)
     **kwargs : dict
-        Additional keyword arguments passed to parquet engine of choice.
+        Additional keyword arguments passed to fastparquet.
     """
     if "refs" in refs:
         refs = refs["refs"]
@@ -131,7 +131,13 @@ def refs_to_dataframe(
     fs.makedirs(url, exist_ok=True)
     fields = get_variables(refs, consolidated=True)
     # write into .zmetadata at top level, one fewer read on access
-    refs[".zmetadata"]['row_group_size'] = row_group_size
+    refs[".zmetadata"]['record_size'] = record_size
+
+    # Initialize arrays
+    paths = np.full(record_size, np.nan, dtype="O")
+    offsets = np.zeros(record_size, dtype="int64")
+    sizes = np.zeros(record_size, dtype="int64")
+    raws = np.full(record_size, np.nan, dtype="O")
     for field in fields:
         field_path = "/".join([url, field])
         if field.startswith("."):
@@ -148,30 +154,86 @@ def refs_to_dataframe(
         if chunk_sizes.size == 0:
             chunk_sizes = np.array([0])
         nchunks = int(np.product(chunk_sizes))
-        extra_rows = row_group_size - nchunks % row_group_size
-        output_size = nchunks + extra_rows
-
-        paths = np.full(output_size, np.nan, dtype="O")
-        offsets = np.zeros(output_size, dtype="int64")
-        sizes = np.zeros(output_size, dtype="int64")
-        raws = np.full(output_size, np.nan, dtype="O")
+        nrec = nchunks // record_size
+        rem = nchunks % record_size
         nmissing = 0
-
+        nraw = 0
+        npath = 0
+        irec = 0
         for i, ind in enumerate(np.ndindex(tuple(chunk_sizes.astype(int)))):
             chunk_id = ".".join([str(ix) for ix in ind])
             key = f"{field}/{chunk_id}"
+            # Last parquet record can be smaller than record_size
+            output_size = record_size if irec < nrec - 1 else rem
+            j = i % record_size
             # Make note if expected number of chunks differs from actual
             # number found in references
             if key in refs:
                 data = refs[key]
                 if isinstance(data, list):
-                    paths[i] = data[0]
-                    offsets[i] = data[1]
-                    sizes[i] = data[2]
+                    npath += 1
+                    paths[j] = data[0]
+                    offsets[j] = data[1]
+                    sizes[j] = data[2]
                 else:
-                    raws[i] = _proc_raw(data)
+                    nraw += 1
+                    raws[j] = _proc_raw(data)
             else:
                 nmissing += 1
+            if j == output_size - 1:
+                # The convention for parquet files is
+                # <url>/<field_name>/refs.<rec_num>.parq
+                out_path = "/".join([field_path, f"refs.{irec}.parq"])
+                if nraw == output_size:
+                    # All raw refs, so we can drop path/offset/size
+                    df = pd.DataFrame(dict(raw=raws), copy=False)
+                    object_encoding = dict(raw="bytes")
+                    has_nulls = False
+                else:
+                    paths_maybe_cat = pd.Series(paths)
+                    nunique = paths_maybe_cat.nunique()
+                    if nunique and npath / nunique >= categorical_threshold:
+                        paths_maybe_cat = paths_maybe_cat.astype("category")
+                    if nraw == 0:
+                        # No raw refs
+                        df = pd.DataFrame(
+                            dict(path=paths_maybe_cat, offset=offsets, size=sizes), 
+                            copy=False
+                        )
+                        object_encoding = dict(path="utf8")
+                        has_nulls = ["path"] if npath != output_size else False
+                    else:
+                        df = pd.DataFrame(
+                            dict(path=paths_maybe_cat, offset=offsets, size=sizes,
+                                 raw=raws), 
+                            copy=False
+                        )
+                        object_encoding = dict(raw="bytes", path="utf8")
+                        has_nulls = ["path", "raw"]
+                        
+                # Subset df if selection is smaller than record size
+                if output_size != record_size:
+                    df = df.iloc[:output_size]
+                    
+                df.to_parquet(
+                    out_path,
+                    engine="fastparquet",
+                    storage_options=storage_options,
+                    compression="zstd",
+                    index=False,
+                    stats=False,
+                    object_encoding=object_encoding,
+                    has_nulls=has_nulls,
+                    **kwargs,
+                )
+                # Reinitialize arrays for next batch of refs to process.
+                paths[:] = np.nan
+                offsets[:] = 0
+                sizes[:] = 0
+                raws[:] = np.nan
+                irec += 1
+                nraw = 0
+                npath = 0
 
         if nmissing:
             # comment: missing keys are fine, so long as they are not a large fraction.
@@ -180,23 +242,3 @@ def refs_to_dataframe(
                 f"Warning: Chunks missing for field {field}. "
                 f"Expected: {nchunks}, Found: {nchunks - nmissing}"
             )
-        # The convention for parquet files is
-        # <url>/<field_name>/refs.parq
-        out_path = "/".join([field_path, "refs.parq"])
-        df = pd.DataFrame(
-            dict(path=paths, offset=offsets, size=sizes, raw=raws), copy=False
-        )
-        kwargs.update(
-            row_group_offsets=row_group_size,
-            object_encoding=dict(raw="bytes", path="utf8"),
-            has_nulls=["path", "raw"],
-            stats=False,
-        )
-        df.to_parquet(
-            out_path,
-            engine="fastparquet",
-            storage_options=storage_options,
-            compression="zstd",
-            index=False,
-            **kwargs,
-        )
