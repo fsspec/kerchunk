6c721d401195389711b5195ec63feb249c55b9bc
Max Jones <14077947+maxrjones@users.noreply.github.com>
2023-10-11 15:51:29 -0400

    Test with only storage_options

--- kerchunk/xarray_backend.py
+++ kerchunk/xarray_backend.py
@@ -8,21 +8,22 @@ class KerchunkBackend(BackendEntrypoint):
     def open_dataset(
         self,
         filename_or_obj,
+        *,
         drop_variables=None,
         storage_options=None,
-        open_dataset_options=None,
     ):
 
         ref_ds = open_reference_dataset(
-            filename_or_obj, storage_options, open_dataset_options
+            filename_or_obj,
+            storage_options=storage_options,
         )
         if drop_variables is not None:
             ref_ds = ref_ds.drop_vars(drop_variables)
+        return ref_ds
 
     open_dataset_parameters = [
         "filename_or_obj",
         "storage_options",
-        "open_dataset_options",
     ]
 
     def guess_can_open(self, filename_or_obj):
@@ -36,8 +37,9 @@ class KerchunkBackend(BackendEntrypoint):
     url = "https://fsspec.github.io/kerchunk/"
 
 
-def open_reference_dataset(filename_or_obj, storage_options, open_dataset_options):
-    fs = ReferenceFileSystem(fo=filename_or_obj)
-
+def open_reference_dataset(
+    filename_or_obj, *, storage_options=None
+):
+    fs = ReferenceFileSystem(fo=filename_or_obj, **storage_options)
     m = fs.get_mapper()
-    return xr.open_dataset(m, engine="zarr", consolidated=False, **open_dataset_options)
+    return xr.open_dataset(m, engine="zarr", consolidated=False)
